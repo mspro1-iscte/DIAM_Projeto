7f14d39b202f99b544763e00443cbf638084d7a7
Miguel <mspro1@iscte-iul.pt>
2022-05-05 12:54:40 +0100

    Day 7 - reorganizing views

--- Projeto/Merch_ISCTE/loja/views.py
+++ Projeto/Merch_ISCTE/loja/views.py
@@ -11,6 +11,8 @@ from django.contrib.auth.decorators import login_required, permission_required, 
 from .models import Cliente, Produto, Categoria
 
 
+# --------------------- Base / Login ---------------------
+
 def index(request):
     lista_categoria = Categoria.objects.all()
     lista_produto = Produto.objects.all()
@@ -39,89 +41,6 @@ def registaruser(request):
     return render(request, 'loja/registaruser.html')
 
 
-def detalhe_produto(request, produto_id):
-    produto = get_object_or_404(Produto, pk=produto_id)
-    return render(request, 'loja/detalhe_produto.html', {'produto': produto})
-
-
-def detalhe_categoria(request, categoria_id):
-    categoria = get_object_or_404(Categoria, pk=categoria_id)
-    lista_categoria = Categoria.objects.all()
-    lista_produto = []
-    for p in Produto.objects.all():
-        if p.categoria_id == categoria_id:
-            lista_produto.append(p)
-    context = {'lista_produto': lista_produto, 'categoria': categoria,'lista_categoria': lista_categoria}
-    return render(request, 'loja/detalhe_categoria.html', context)
-
-
-def carrinho(request):
-    lista_ids = request.session['lista_carrinho']
-    lista_carrinho = []
-    for id in lista_ids:
-        produto = get_object_or_404(Produto, pk=id)
-        lista_carrinho.append(produto)
-    context = {'lista_carrinho': lista_carrinho}
-    return render(request, 'loja/carrinho.html', context)
-
-
-def nova_categoria(request):
-    categoria_nome = request.POST['categoria_nome']
-    if bool(request.FILES.get('categoria_file', False)):
-        myfile = request.FILES['categoria_file']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)[5:]  # remover /loja
-        categoria = Categoria(categoria_nome=categoria_nome, foto=uploaded_file_url)
-    else:
-        categoria = Categoria(categoria_nome=categoria_nome)
-    categoria.save()
-    return HttpResponseRedirect(reverse('loja:index'))
-
-
-def novo_produto(request, categoria_id):
-    produto_nome = request.POST['produto_nome']
-    produto_texto = request.POST['produto_texto']
-    preco_data = request.POST['preco_data']
-    cat = get_object_or_404(Categoria, pk=categoria_id)
-    if bool(request.FILES.get('produto_file', False)):
-        myfile = request.FILES['produto_file']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)[5:]  # remover /loja
-        print(uploaded_file_url)
-        produto = Produto(produto_nome=produto_nome, produto_texto=produto_texto, preco_data=preco_data,
-                          categoria=cat, foto=uploaded_file_url)
-
-    else:
-        produto = Produto(produto_nome=produto_nome, produto_texto=produto_texto, preco_data=preco_data,
-                          categoria=cat)
-    produto.save()
-
-    return HttpResponseRedirect(reverse('loja:index'))
-
-
-def adicionar_carrinho(request, produto_id):
-
-    if not 'lista_carrinho' in request.session or not request.session['lista_carrinho']:
-        request.session['lista_carrinho'] = [produto_id]
-
-    else:
-        lista_carrinho = request.session['lista_carrinho']
-        lista_carrinho.append(produto_id)
-        request.session['lista_carrinho'] = lista_carrinho
-    print(request.session['lista_carrinho'])
-    return HttpResponseRedirect(reverse('loja:index'))
-    #return render(request, 'loja/detalhe_produto.html', {'produto': produto})
-
-
-def remover_carrinho(request, produto_id):
-    lista_carrinho = request.session['lista_carrinho']
-    lista_carrinho.remove(produto_id)
-    request.session['lista_carrinho'] = lista_carrinho
-    return HttpResponseRedirect(reverse('loja:carrinho'))
-
-
 def registo(request):
     username = request.POST['username']
     password = request.POST['pass']
@@ -147,10 +66,12 @@ def perfil(request):
     return render(request, 'loja/perfil.html', {'cliente': cliente})
 
 
-def apagar_categoria(request, categoria_id):
-    record = Categoria.objects.get(id=categoria_id)
-    record.delete()
-    return HttpResponseRedirect(reverse('loja:index'))
+# --------------------- Produto ---------------------
+
+def criar_produto(request, categoria_id):
+    categoria = get_object_or_404(Categoria, pk=categoria_id)
+    context = {'categoria': categoria}
+    return render(request, 'loja/criar_produto.html', context)
 
 
 def apagar_produto(request, produto_id):
@@ -159,11 +80,98 @@ def apagar_produto(request, produto_id):
     return HttpResponseRedirect(reverse('loja:index'))
 
 
+def detalhe_produto(request, produto_id):
+    produto = get_object_or_404(Produto, pk=produto_id)
+    return render(request, 'loja/detalhe_produto.html', {'produto': produto})
+
+
+def novo_produto(request, categoria_id):
+    produto_nome = request.POST['produto_nome']
+    produto_texto = request.POST['produto_texto']
+    preco_data = request.POST['preco_data']
+    cat = get_object_or_404(Categoria, pk=categoria_id)
+    if bool(request.FILES.get('produto_file', False)):
+        myfile = request.FILES['produto_file']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)[5:]  # remover /loja
+        print(uploaded_file_url)
+        produto = Produto(produto_nome=produto_nome, produto_texto=produto_texto, preco_data=preco_data,
+                          categoria=cat, foto=uploaded_file_url)
+
+    else:
+        produto = Produto(produto_nome=produto_nome, produto_texto=produto_texto, preco_data=preco_data,
+                          categoria=cat)
+    produto.save()
+
+    return HttpResponseRedirect(reverse('loja:index'))
+
+
+# --------------------- Categoria ---------------------
+
 def criar_categoria(request):
     return render(request, 'loja/criar_categoria.html')
 
 
-def criar_produto(request, categoria_id):
+def apagar_categoria(request, categoria_id):
+    record = Categoria.objects.get(id=categoria_id)
+    record.delete()
+    return HttpResponseRedirect(reverse('loja:index'))
+
+
+def detalhe_categoria(request, categoria_id):
     categoria = get_object_or_404(Categoria, pk=categoria_id)
-    context = {'categoria': categoria}
-    return render(request, 'loja/criar_produto.html', context)
+    lista_categoria = Categoria.objects.all()
+    lista_produto = []
+    for p in Produto.objects.all():
+        if p.categoria_id == categoria_id:
+            lista_produto.append(p)
+    context = {'lista_produto': lista_produto, 'categoria': categoria, 'lista_categoria': lista_categoria}
+    return render(request, 'loja/detalhe_categoria.html', context)
+
+
+def nova_categoria(request):
+    categoria_nome = request.POST['categoria_nome']
+    if bool(request.FILES.get('categoria_file', False)):
+        myfile = request.FILES['categoria_file']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)[5:]  # remover /loja
+        categoria = Categoria(categoria_nome=categoria_nome, foto=uploaded_file_url)
+    else:
+        categoria = Categoria(categoria_nome=categoria_nome)
+    categoria.save()
+    return HttpResponseRedirect(reverse('loja:index'))
+
+
+# --------------------- Carrinho ---------------------
+
+def carrinho(request):
+    lista_ids = request.session['lista_carrinho']
+    lista_carrinho = []
+    for id in lista_ids:
+        produto = get_object_or_404(Produto, pk=id)
+        lista_carrinho.append(produto)
+    context = {'lista_carrinho': lista_carrinho}
+    return render(request, 'loja/carrinho.html', context)
+
+
+def adicionar_carrinho(request, produto_id):
+    if not 'lista_carrinho' in request.session or not request.session['lista_carrinho']:
+        request.session['lista_carrinho'] = [produto_id]
+
+    else:
+        lista_carrinho = request.session['lista_carrinho']
+        lista_carrinho.append(produto_id)
+        request.session['lista_carrinho'] = lista_carrinho
+    print(request.session['lista_carrinho'])
+    return HttpResponseRedirect(reverse('loja:index'))
+    # return render(request, 'loja/detalhe_produto.html', {'produto': produto})
+
+
+def remover_carrinho(request, produto_id):
+    lista_carrinho = request.session['lista_carrinho']
+    lista_carrinho.remove(produto_id)
+    request.session['lista_carrinho'] = lista_carrinho
+    return HttpResponseRedirect(reverse('loja:carrinho'))
+
